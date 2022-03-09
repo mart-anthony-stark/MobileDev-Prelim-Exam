a366953a162ad2f11c505d080a4eff1df4cb0dad
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-09 14:27:22 +0800

    add mart profile activity

---
+++ app/src/main/java/com/group8/listapp/MartActivity.java
@@ -0,0 +1,14 @@
+package com.group8.listapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MartActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mart);
+    }
+    
+}

b038d20ba356b1e12ff25745d2466e713ed0b972
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-10 19:52:44 +0800

    add navigate up

--- app/src/main/java/com/group8/listapp/MartActivity.java
+++ app/src/main/java/com/group8/listapp/MartActivity.java
@@ -9,6 +9,13 @@ public class MartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mart);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    
+
+	@Override
+	public boolean onNavigateUp(){
+		// TODO: Implement this method
+		finish();
+		return true;
+	}
 }
